71fcd5abc91c8d626d3066bd470caae415b12ce0
unknown <seppe.bellens@hotmail.nl>
2023-08-20 19:22:02 +0200

    Build[] layout 3d donut

--- donut.js
+++ donut.js
@@ -1,32 +1,34 @@
 import * as THREE from 'three';
-
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.132.2/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.132.2/examples/jsm/loaders/GLTFLoader.js';
-
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+ 
+// GLTFLoader
+let donut = null;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
 
 const renderer = new THREE.WebGLRenderer();
-const donut3dDiv = document.querySelector('.donut3d');
-renderer.setSize(donut3dDiv.clientWidth, donut3dDiv.clientHeight);
-donut3dDiv.appendChild(renderer.domElement);
+document.querySelector(".donut3d")
+renderer.setSize(document.querySelector(".donut3d").clientWidth, document.querySelector(".donut3d").clientHeight);
+document.querySelector(".donut3d").appendChild(renderer.domElement);
+// document.body.appendChild(renderer.domElement);
 
+// add light
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+// load donut.glb
 const loader = new GLTFLoader();
-
-// Load the donut model
 loader.load("donut.glb", function (gltf) {
-	// Get the loaded donut scene
-	const donut = gltf.scene;
-
-	// Scale and position the donut
-	donut.scale.set(10, 10, 10);
-	donut.position.set(0, 0, 0); // Adjust the position as needed
-
-	// Add the donut to the scene
-	scene.add(donut);
-
-	// Call the animate function to start rendering
-	animate();
+	// scale
+	donut = gltf;
+	donut.scene.scale.set(10, 10, 10);
+	scene.add(donut.scene);
 });
 
 camera.position.z = 5;
@@ -34,14 +36,8 @@ camera.position.z = 5;
 function animate() {
 	requestAnimationFrame(animate);
 
-	// Update the donut's rotation
-	if (scene.children.length > 0) {
-		const donut = scene.children[0];
-		donut.rotation.x += 0.01;
-		donut.rotation.y += 0.01;
-	}
-
 	renderer.render(scene, camera);
 }
 
 animate();
+
