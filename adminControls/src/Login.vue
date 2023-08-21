97af9e3a91f221ee4dae6cda5282d778926873a6
unknown <seppe.bellens@hotmail.nl>
2023-08-21 16:23:49 +0200

    Build[] setup login

---
+++ adminControls/src/Login.vue
@@ -0,0 +1,40 @@
+<!-- import components -->
+
+<template>
+  <h1>Login here</h1>
+
+  <label for="username">username:</label><br />
+  <input type="text" id="username" name="username" required /><br /><br />
+
+  <label for="password">password:</label><br />
+  <input type="text" id="password" name="password" required /><br /><br />
+</template>
+
+<!-- <style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style> -->
